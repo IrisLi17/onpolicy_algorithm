745b87fdb6261ad197839f887568c59b30dd1912
Yunfei Li <yunfeili.cloud@gmail.com>
2022-04-18 11:20:14 +0800

    update config

--- config/isaac/panda_push_joint_config.py
+++ config/isaac/panda_push_joint_config.py
@@ -10,7 +10,7 @@ class PushConfig(BaseConfig):
         seed = 42
         num_envs = 1024
         # num_observations = 3 * 224 * 224 + 12
-        num_observations = 7 + 27
+        num_observations = 3 + 27 + 3
         num_actions = 8
         max_episode_length = 100
     
@@ -30,9 +30,9 @@ class PushConfig(BaseConfig):
 config = dict(
     env_id="IsaacPandaPushState-v0",
     algo="ppo",
-    name="test_joint_decimal6_1024w_step64_penori_saturate_horizon100_debug",
+    name="test_joint_obshand_noquat_reset",
     # name="test_joint_decimal6_1024w_step64_dense",
-    total_timesteps=int(1e8),
+    total_timesteps=int(5e7),
     entry_point=PandaPushEnv,
     env_config=PushConfig(),
     policy_type="mlp",
