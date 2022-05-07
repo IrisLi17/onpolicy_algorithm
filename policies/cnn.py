16b9bacf46144931c6dc07d459e0fd54e8b16f04
Yunfei Li <yunfeili.cloud@gmail.com>
2022-05-07 17:27:25 +0800

    Dagger + lstm policy

--- policies/cnn.py
+++ policies/cnn.py
@@ -92,3 +92,91 @@ class CNNStatePolicy(ActorCriticPolicy):
         log_probs = torch.sum(action_dist.log_prob(actions), dim=-1, keepdim=True)
         entropy = action_dist.entropy()
         return log_probs, entropy, rnn_hxs
+
+
+class CNNStateHistoryPolicy(ActorCriticPolicy):
+    def __init__(self, image_shape, state_dim, action_dim, hidden_size, lstm_hidden_size) -> None:
+        super().__init__()
+        assert len(image_shape) == 3
+        assert image_shape[1] == image_shape[2]
+        self.image_shape = image_shape  # (C, H, W)
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.hidden_size = hidden_size
+
+        # Recurrent perception part
+        self.image_encoder = nn.Sequential(
+            nn.Conv2d(self.image_shape[0], 2 * self.image_shape[0], 8, 4, 0),
+            nn.ReLU(),
+            nn.Conv2d(2 * self.image_shape[0], 4 * self.image_shape[0], 4, 2, 0),
+            nn.ReLU(),
+            nn.Conv2d(4 * self.image_shape[0], 4 * self.image_shape[0], 3, 1, 0),
+            nn.ReLU(), nn.Flatten(),
+        )
+        with torch.no_grad():
+            image_feature_dim = self.image_encoder(torch.zeros(1, *self.image_shape)).shape[-1]
+        self.image_projector = nn.Linear(image_feature_dim, hidden_size)
+        self.state_encoder = nn.Sequential(
+            nn.Linear(self.state_dim, hidden_size), nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size)
+        )
+        self.memory_cell = nn.LSTMCell(2 * hidden_size, lstm_hidden_size)
+        self.pi_mean_layers = nn.Sequential(
+            nn.Linear(lstm_hidden_size, hidden_size), nn.ReLU(),
+            nn.Linear(hidden_size, self.action_dim)
+        )
+        self.pi_logstd = nn.Parameter(torch.zeros(self.action_dim), requires_grad=True)
+        self.vf_layers = nn.Sequential(
+            nn.Linear(lstm_hidden_size, hidden_size), nn.ReLU(),
+            nn.Linear(hidden_size, 1)
+        )
+        self.is_recurrent = True
+        self.recurrent_hidden_state_size = 2 * lstm_hidden_size
+        self._initialize()
+    
+    def _initialize(self):
+        for net in self.vf_layers:
+            if isinstance(net, nn.Linear):
+                nn.init.orthogonal_(net.weight, gain=np.sqrt(2))
+                nn.init.constant_(net.bias, 0.)
+        nn.init.orthogonal_(self.pi_mean_layers[-1].weight, gain=0.01)
+        nn.init.constant_(self.pi_mean_layers[-1].bias, 0.)
+    
+    def forward(self, obs, rnn_hxs=None, rnn_masks=None):
+        assert obs.shape[-1] == torch.prod(torch.tensor(self.image_shape)) + self.state_dim
+        image_obs = torch.narrow(obs, dim=1, start=0, length=torch.prod(torch.tensor(self.image_shape))).reshape((-1, *self.image_shape))
+        state_obs = torch.narrow(obs, dim=1, start=torch.prod(torch.tensor(self.image_shape)), length=self.state_dim)
+        image_feature = self.image_projector(self.image_encoder(image_obs))
+        state_feature = self.state_encoder(state_obs)
+        input_feature = torch.cat([image_feature, state_feature], dim=-1)
+        N = rnn_hxs.shape[0]
+        T = obs.shape[0] // N
+        input_feature = input_feature.view(T, N, -1)
+        rnn_masks = rnn_masks.view(T, N, 1)
+        lstm_hxs = torch.narrow(rnn_hxs, dim=1, start=0, length=self.recurrent_hidden_state_size // 2)
+        lstm_cell = torch.narrow(rnn_hxs, dim=1, start=self.recurrent_hidden_state_size // 2, length=self.recurrent_hidden_state_size // 2)
+        output_feature = []
+        for t in range(T):
+            lstm_hxs, lstm_cell = self.memory_cell(input_feature[t], (lstm_hxs * rnn_masks[t], lstm_cell))
+            output_feature.append(lstm_hxs)
+        output_feature = torch.stack(output_feature, dim=0).reshape((T * N, -1))
+        rnn_hxs = torch.cat([lstm_hxs, lstm_cell], dim=-1)
+        action_mean = self.pi_mean_layers(output_feature)
+        action_dist = torch.distributions.Normal(action_mean, torch.exp(self.pi_logstd))
+        value = self.vf_layers(output_feature)
+        return value, action_dist, rnn_hxs
+    
+    def act(self, obs, rnn_hxs=None, rnn_masks=None, deterministic=False):
+        value, action_dist, rnn_hxs = self.forward(obs, rnn_hxs, rnn_masks)
+        if deterministic:
+            action = action_dist.mean
+        else:
+            action = action_dist.sample()
+        log_probs = torch.sum(action_dist.log_prob(action), dim=-1, keepdim=True)
+        return value, action, log_probs, rnn_hxs
+    
+    def evaluate_actions(self, obs, rnn_hxs=None, rnn_masks=None, actions=None):
+        _, action_dist, rnn_hxs = self.forward(obs, rnn_hxs, rnn_masks)
+        log_probs = torch.sum(action_dist.log_prob(actions), dim=-1, keepdim=True)
+        entropy = action_dist.entropy()
+        return log_probs, entropy, rnn_hxs
