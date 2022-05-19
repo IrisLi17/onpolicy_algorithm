64871670d9606a983602c519df7da786b77588fe
Yunfei Li <yunfeili.cloud@gmail.com>
2022-05-19 07:59:43 -0400

    Update lstm config

---
+++ config/isaac/panda_push_cartesian_cnnlstm_config.py
@@ -0,0 +1,122 @@
+import sys
+sys.path.append("../isaac_projects/panda-isaac")
+from panda_isaac.panda_push import PandaPushEnv
+from panda_isaac.base_config import BaseConfig
+sys.path.remove("../isaac_projects/panda-isaac")
+
+
+STATE_HISTORY_LENGTH = 1
+IMAGE_HISTORY_LENGTH = 1
+
+
+class PushConfig(BaseConfig):
+    class env(BaseConfig.env):
+        seed = 42
+        num_envs = 256
+        num_observations = IMAGE_HISTORY_LENGTH * 3 * 84 * 84 + STATE_HISTORY_LENGTH * 15
+        num_actions = 4
+        num_state_obs = 18 * STATE_HISTORY_LENGTH
+        max_episode_length = 100
+    
+    class asset(BaseConfig.asset):
+        robot_urdf = "urdf/franka_description/robots/franka_panda_cam.urdf"
+    
+    class obs(BaseConfig.obs):
+        type = "pixel"
+        im_size = 84
+        history_length = IMAGE_HISTORY_LENGTH
+        state_history_length = STATE_HISTORY_LENGTH
+        noise = True
+    
+    class cam(BaseConfig.cam):
+        view = "ego"
+        # view = "third"
+        fov = 86
+        w = 149
+        h = 84
+        # loc_r = [180, -45.0, 180.0]
+        # loc_p = [0.11104 - 0.0592106 - 0.01, -0.0156, 0.015]
+    
+    class control(BaseConfig.control):
+        decimal = 6
+        controller = "ik"
+        # controller = "osc"
+        # common_speed = 2.17 * 1
+        # common_speed = 1.5 * decimal
+    
+    class reward(BaseConfig.reward):
+        type = "dense"
+        contact_coef = -0.1
+    
+    class safety(BaseConfig.safety):
+        brake_on_contact = False
+        contact_force_th = 30.0
+
+    # class domain_randomization:
+    #     friction_range = [0.5, 1.5]
+
+
+def goal_in_air_cl(_locals, _globals):
+    if _locals["j"] > 1:
+        import torch
+        ep_infos = _locals["ep_infos"]
+        success_rate = torch.mean(torch.tensor(ep_infos["is_success"]).float()).item()
+        cur_goal_in_air_ratio = _locals["self"].env.goal_in_air
+        if success_rate > 0.6:
+            _locals["self"].env.set_goal_in_air_ratio(min(cur_goal_in_air_ratio + 0.1, 1.0))
+        print("Goal in air ratio =", _locals["self"].env.goal_in_air)
+
+
+def contact_force_th_cl(_locals, _globals):
+    if _locals["j"] > 1:
+        import torch
+        ep_infos = _locals["ep_infos"]
+        success_rate = torch.mean(torch.tensor(ep_infos["is_success"]).float()).item()
+        cur_contact_force_th = _locals["self"].env.cfg.safety.contact_force_th
+        if success_rate > 0.6:
+            _locals["self"].env.set_contact_force_th(max(0.5 * cur_contact_force_th, 5.0))
+        print("Contact force threshold =", _locals["self"].env.cfg.safety.contact_force_th)
+
+
+config = dict(
+    env_id="IsaacPandaPushCNN-v0",
+    algo="ppo",
+    name="lstm_oldcam_dagger_aux0.3_prvlg_statecritic_noldr_th30cl0.6_final5_pen-0.1",
+    # name="1i1s_oldcam_bc30_statenoise_camurdf_contact-0.1",
+    # name="test_joint_decimal6_1024w_step64_dense",
+    total_timesteps=int(2e7),
+    entry_point=PandaPushEnv,
+    env_config=PushConfig(),
+    sim_device="cuda:1",
+    # policy_type=("policies.cnn", "CNNStatePolicy"),
+    # policy=dict(
+    #     image_shape=(IMAGE_HISTORY_LENGTH, 3, 84, 84), 
+    #     state_dim=15 * STATE_HISTORY_LENGTH, 
+    #     action_dim=4,
+    #     hidden_size=64,
+    #     # num_bin=21,
+    # ),
+    policy_type=("policies.cnn", "CNNStateHistoryPolicy"),
+    policy=dict(
+        image_shape=(3, 84, 84),
+        state_dim=15,
+        action_dim=4,
+        hidden_size=64,
+        lstm_hidden_size=64,
+        previ_dim=3,
+        state_only_critic=True,
+    ),
+    train=dict(
+      n_steps=128,
+      nminibatches=16,
+      # learning_rate=1e-3,
+      learning_rate=1.5e-4,
+      # cliprange=0.1,
+      n_imitation_epoch=30,
+      dagger=True,
+      aux_loss_coef=0.3,
+      previlege_critic=True,
+      use_wandb=True
+    ),
+    callback=[contact_force_th_cl],
+)
--- plot/plot_compare.py
+++ plot/plot_compare.py
@@ -2,12 +2,16 @@ import sys, os
 import numpy as np
 import pandas
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
 
 
 if __name__ == '__main__':
     option = sys.argv[1]
     log_paths = sys.argv[2:]
-    window = 20
+    # assert option in ['success_rate', 'eval', 'entropy', 'aug_ratio', 'self_aug_ratio']
+    window = 1
+    L = 10000
     def get_item(log_file, label):
         data = pandas.read_csv(log_file, index_col=None, comment='#', error_bad_lines=True)
         return data[label].values
@@ -17,16 +21,68 @@ if __name__ == '__main__':
             out[i] = np.mean(array[i:i + window])
         return out
     fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-    for log_path in log_paths:
+    for idx, log_path in enumerate(log_paths):
         progress_file = os.path.join(log_path, 'progress.csv')
-        data = get_item(progress_file, option)
+        eval_file = os.path.join(log_path, 'eval.csv')
+        # if 'ds' in log_path:
+        #     success_rate = get_item(progress_file, 'ep_success_rate')
+        # else:
+        # success_rate = get_item(progress_file, 'is_success')
         total_timesteps = get_item(progress_file, 'total_timesteps')
+        try:
+            eval_reward = get_item(eval_file, 'mean_eval_reward')
+            n_update = get_item(eval_file, 'n_updates')
+        except:
+            pass
         # success_rate = smooth(success_rate, window)
         # total_timesteps = smooth(total_timesteps, window)
-        ax.plot(smooth(total_timesteps, window), smooth(data, window), label=log_path)
-    ax.set_title(option)
+        if option == 'success_rate':
+            print("label", log_path[-50:])
+            success_rate = get_item(progress_file, "is_success")
+            if idx == 0:
+                success_rate = success_rate[0:]
+            ax.plot(smooth(total_timesteps, window), smooth(success_rate, window), label=log_path[-50:])
+        elif option == 'eval':
+            # ax[0].plot(n_updates*65536, eval_reward, label=log_path)
+            
+            ax[0].plot(smooth(total_timesteps[n_updates-1], window), smooth(eval_reward, window), label=log_path)
+        # elif option == 'entropy':
+        #     entropy = get_item(progress_file, 'policy_entropy')
+        #     ax[0].plot(smooth(total_timesteps, window), smooth(entropy, window), label=log_path)
+        elif option == 'aug_ratio':
+            original_success = get_item(progress_file, 'original_success')
+            total_success = get_item(progress_file, 'total_success')
+            aug_ratio = (total_success - original_success) / (total_success + 1e-8)
+            print(total_timesteps.shape, aug_ratio.shape)
+            ax[0].plot(smooth(total_timesteps, 2), smooth(aug_ratio, 2), label=log_path)
+        elif option == 'self_aug_ratio':
+            self_aug_ratio = get_item(progress_file, 'self_aug_ratio')
+            ax[0].plot(smooth(total_timesteps, window), smooth(self_aug_ratio, window), label=log_path)
+        else:
+            values = get_item(progress_file, option)
+            # ax.plot(n_update, values, label=log_path[-50:])
+            ax.plot(total_timesteps[:L], values[:L], label=log_path[-50:])
+        try:
+            original_steps = get_item(progress_file, 'original_timesteps')[0]
+            augment_steps = get_item(progress_file, 'augment_steps') / original_steps
+            # augment_steps = smooth(augment_steps, window)
+        except:
+            augment_steps = np.zeros(total_timesteps.shape)
+        # ax[1].plot(smooth(total_timesteps, window), smooth(augment_steps, window), label=log_path)
+    if option == 'success_rate':
+        ax.set_title('success rate')
+    elif option == 'eval':
+        ax[0].set_title('eval success rate')
+    # elif option == 'entropy':
+    #     ax[0].set_title('entropy')
+    elif option == 'aug_ratio':
+        ax[0].set_title('aug success episode / total success episode')
+    elif option == 'self_aug_ratio':
+        ax[0].set_title('self_aug_ratio')
+    # ax[1].set_title('augment steps / original rollout steps')
     ax.grid()
-    plt.legend()
+    # ax[1].grid()
+    plt.legend(loc="best")
     plt.savefig("figure.png")
-    plt.show()
+    # plt.show()
     
