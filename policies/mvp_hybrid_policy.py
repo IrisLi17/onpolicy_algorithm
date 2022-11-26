95259f55359a1d48e6695bb182184952b2d49afb
Yunfei Li <yunfeili.cloud@gmail.com>
2022-11-26 21:58:16 +0800

    do not compute prob for unused param

--- policies/mvp_hybrid_policy.py
+++ policies/mvp_hybrid_policy.py
@@ -1,4 +1,5 @@
 from policies.base import ActorCriticPolicy
+import numpy as np
 import torch
 import torch.nn as nn
 from torch.distributions.categorical import Categorical
@@ -20,21 +21,33 @@ class HybridMlpPolicy(ActorCriticPolicy):
         self.act_type = nn.Sequential(
             nn.Linear(2 * proj_img_dim + proj_state_dim, hidden_dim), nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
             nn.Linear(hidden_dim, n_primitive),
         )
         self.act_param = nn.ModuleList([nn.Sequential(
             nn.Linear(2 * proj_img_dim + proj_state_dim, hidden_dim), nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
             nn.Linear(hidden_dim, num_bin)
         ) for _ in range(act_dim)])
         self.value_layers = nn.Sequential(
             nn.Linear(2 * proj_img_dim + proj_state_dim, hidden_dim), nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
             nn.Linear(hidden_dim, 1)
         )
         self.is_recurrent = False
         self.recurrent_hidden_state_size = 1
+        self.init_weights(self.act_type, [np.sqrt(2), np.sqrt(2), np.sqrt(2), 0.01])
+        for i in range(act_dim):
+            self.init_weights(self.act_param[i], [np.sqrt(2), np.sqrt(2), np.sqrt(2), 0.01])
+        self.init_weights(self.value_layers, [np.sqrt(2), np.sqrt(2), np.sqrt(2), 1.0])
     
+    @staticmethod
+    def init_weights(sequential, scales):
+        [torch.nn.init.orthogonal_(module.weight, gain=scales[idx]) for idx, module in
+         enumerate(mod for mod in sequential if isinstance(mod, nn.Linear))]
+
     def _obs_parser(self, obs):
         cur_img_feat = torch.narrow(obs, dim=-1, start=0, length=self.mvp_feat_dim)
         cur_state = torch.narrow(obs, dim=-1, start=self.mvp_feat_dim, length=self.state_obs_dim)
@@ -63,7 +76,11 @@ class HybridMlpPolicy(ActorCriticPolicy):
             act_type = act_type_dist.sample().unsqueeze(dim=-1)
             act_params = [act_param_dist[i].sample().unsqueeze(dim=-1) for i in range(len(act_param_dist))]
         act_type_logprob = act_type_dist.log_prob(act_type.squeeze(dim=-1)).unsqueeze(dim=-1)
-        act_param_logprob = [act_param_dist[i].log_prob(act_params[i].squeeze(dim=-1)).unsqueeze(dim=-1) for i in range(self.act_dim)]
+        use_param_mask = (act_type == 0).float().detach()
+        act_param_logprob = [
+            act_param_dist[i].log_prob(act_params[i].squeeze(dim=-1)).unsqueeze(dim=-1) * use_param_mask
+            for i in range(self.act_dim)
+        ]
         act_params = [2 * (act_param / (self.num_bin - 1.0)) - 1 for act_param in act_params]
         actions = torch.cat([act_type] + act_params, dim=-1)
         log_prob = torch.sum(torch.stack([act_type_logprob] + act_param_logprob, dim=-1), dim=-1)
@@ -73,10 +90,14 @@ class HybridMlpPolicy(ActorCriticPolicy):
         _, (act_type_dist, act_param_dist), _ = self.forward(obs, rnn_hxs, rnn_masks)
         act_type = actions[:, 0].int()
         act_params = torch.round((actions[:, 1:] + 1) / 2 * (self.num_bin - 1.0)).int()
+        use_param_mask = (act_type == 0).float()
         act_type_logprob = act_type_dist.log_prob(act_type)
-        act_param_logprob = [act_param_dist[i].log_prob(act_params[:, i]) for i in range(self.act_dim)]
+        act_param_logprob = [
+            act_param_dist[i].log_prob(act_params[:, i]) * use_param_mask
+            for i in range(self.act_dim)
+        ]
         log_prob = torch.sum(torch.stack([act_type_logprob] + act_param_logprob, dim=-1), dim=-1, keepdim=True)
         act_type_ent = act_type_dist.entropy()
-        act_param_ent = [act_param_dist[i].entropy() for i in range(self.act_dim)]
+        act_param_ent = [act_param_dist[i].entropy() * use_param_mask for i in range(self.act_dim)]
         entropy = torch.sum(torch.stack([act_type_ent] + act_param_ent, dim=-1), dim=-1).mean()
         return log_prob, entropy, rnn_hxs
